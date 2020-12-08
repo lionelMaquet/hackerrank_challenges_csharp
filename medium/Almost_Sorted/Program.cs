a20a496de4f5a7b4e571658af06ed6a0ff5464ba
Lionel Maquet <lionel.maquet@gmail.com>
2020-12-08 23:08:28 +0100

    uses proper hackerrank code

--- medium/Almost_Sorted/Program.cs
+++ medium/Almost_Sorted/Program.cs
@@ -1,151 +1,172 @@
-﻿// LINK : https://www.hackerrank.com/challenges/almost-sorted/problem
-
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
 
-namespace Almost_Sorted
+class Solution
 {
-    class Program
+
+    // Complete the almostSorted function below.
+    static void almostSorted(int[] arr)
     {
-        static bool IsArrayAscending(int[] arr)
+
+        if (!IsArrayAscending(arr))
         {
-            if (arr.Length < 2)
+            if (!CheckForPotentialSwap(arr))
             {
-                return true;
-            }
-            for (int i = 1; i < arr.Length; i++)
-            {
-                if (arr[i] < arr[i - 1])
+                if (!CheckForPotentialReverse(arr))
                 {
-                    return false;
+                    Console.WriteLine("no");
                 }
             }
+        }
+        else
+        {
+            Console.WriteLine("Yes");
+        }
+
+
+
+
+
+    }
+
+    static bool IsArrayAscending(int[] arr)
+    {
+        if (arr.Length < 2)
+        {
+            return true;
+        }
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < arr[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool CheckForPotentialSwap(int[] MyArray)
+    {
+        int[] arr = new int[MyArray.Length];
+        MyArray.CopyTo(arr, 0);
+
+        int SmallerFollowingIndex = 0;
+        int ProblemNumberIndex = 0;
+        bool IsProblemNumberSet = false;
+
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+
+            if (arr[i] > arr[i + 1])
+            {
+                ProblemNumberIndex = i;
+                IsProblemNumberSet = true;
+                break;
+            }
+
+        }
+
+
+        int Smallest = arr[ProblemNumberIndex];
+
+        for (int i = ProblemNumberIndex + 1; i < arr.Length; i++)
+        {
+            if (arr[i] < Smallest)
+            {
+                Smallest = arr[i];
+                SmallerFollowingIndex = i;
+            }
+        }
+
+
+
+        int temp = arr[ProblemNumberIndex];
+        arr[ProblemNumberIndex] = arr[SmallerFollowingIndex];
+        arr[SmallerFollowingIndex] = temp;
+
+
+        if (IsArrayAscending(arr))
+        {
+            Console.WriteLine("yes \nswap " + (ProblemNumberIndex + 1).ToString() + " " + (SmallerFollowingIndex + 1).ToString());
             return true;
         }
 
-        static bool CheckForPotentialSwap(int[] MyArray)
+        return false;
+
+    }
+
+    static bool CheckForPotentialReverse(int[] MyArray)
+    {
+
+        int[] arr = new int[MyArray.Length];
+        MyArray.CopyTo(arr, 0);
+
+        int ReverseStartIndex = 0;
+        int ReverseDuration = 1;
+
+        for (int i = 0; i < arr.Length; i++)
         {
-            int[] arr = new int[MyArray.Length];
-            MyArray.CopyTo(arr, 0);
-
-            int SmallerFollowingIndex = 0;
-            int ProblemNumberIndex = 0;
-            bool IsProblemNumberSet = false;
-
-
-            for (int i = 0; i < arr.Length - 1; i++)
+            if (arr[i] > arr[i + 1])
             {
-
-                if (arr[i] > arr[i + 1])
-                {
-                    ProblemNumberIndex = i;
-                    IsProblemNumberSet = true;
-                    break;
-                }
-
+                ReverseStartIndex = i;
+                break;
             }
-
-
-            int Smallest = arr[ProblemNumberIndex];
-
-            for (int i = ProblemNumberIndex + 1; i < arr.Length; i++)
-            {
-                if (arr[i] < Smallest)
-                {
-                    Smallest = arr[i];
-                    SmallerFollowingIndex = i;
-                }
-            }
-
-
-
-            int temp = arr[ProblemNumberIndex];
-            arr[ProblemNumberIndex] = arr[SmallerFollowingIndex];
-            arr[SmallerFollowingIndex] = temp;
-
-
-            if (IsArrayAscending(arr)) {
-                Console.WriteLine("yes \nswap " + (ProblemNumberIndex + 1).ToString() + " " + (SmallerFollowingIndex + 1).ToString());
-                return true;
-            }
-
-            return false;
-            
         }
 
-        static bool CheckForPotentialReverse(int[] MyArray)
+        for (int i = ReverseStartIndex + 1; i < arr.Length; i++)
         {
-
-            int[] arr = new int[MyArray.Length];
-            MyArray.CopyTo(arr, 0);
-
-            int ReverseStartIndex = 0;
-            int ReverseDuration = 1;
-
-            for (int i = 0; i < arr.Length; i++)
+            if (arr[i] < arr[i - 1])
             {
-                if (arr[i] > arr[i + 1])
-                {
-                    ReverseStartIndex = i;
-                    break;
-                }
-            }
-
-            for (int i = ReverseStartIndex + 1; i < arr.Length; i++)
-            {
-                if (arr[i] < arr[i - 1])
-                {
-                    ReverseDuration++;
-                } else
-                {
-                    break;
-                }
-            }
-
-            // reverse from start to end 
-
-            int[] ReversedSub = new int[ReverseDuration];
-            for (int i = 0; i < ReverseDuration; i++)
-            {
-                ReversedSub[ReversedSub.Length - (1 + i)] = arr[ReverseStartIndex + i];
-            }
-
-            for (int i = 0; i < ReverseDuration; i++)
-            {
-                arr[ReverseStartIndex + i] = ReversedSub[i];
-            }
-
-
-            if (IsArrayAscending(arr))
-            {
-                Console.WriteLine("yes \nreverse " + (ReverseStartIndex + 1).ToString() + " " + (ReverseStartIndex + ReverseDuration).ToString());
-                return true;
-            }
-
-            return false;
-
-        }
-
-        static void Main(string[] args)
-        {
-            int[] arr = { 1, 2, 8, 7, 6 };
-
-            if (!IsArrayAscending(arr))
-            {
-                if (!CheckForPotentialSwap(arr))
-                {
-                    if (!CheckForPotentialReverse(arr))
-                    {
-                        Console.WriteLine("no");
-                    }
-                }
+                ReverseDuration++;
             }
             else
             {
-                Console.WriteLine("Yes");
+                break;
             }
-
-            
-
         }
+
+        // reverse from start to end 
+
+        int[] ReversedSub = new int[ReverseDuration];
+        for (int i = 0; i < ReverseDuration; i++)
+        {
+            ReversedSub[ReversedSub.Length - (1 + i)] = arr[ReverseStartIndex + i];
+        }
+
+        for (int i = 0; i < ReverseDuration; i++)
+        {
+            arr[ReverseStartIndex + i] = ReversedSub[i];
+        }
+
+
+        if (IsArrayAscending(arr))
+        {
+            Console.WriteLine("yes \nreverse " + (ReverseStartIndex + 1).ToString() + " " + (ReverseStartIndex + ReverseDuration).ToString());
+            return true;
+        }
+
+        return false;
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        almostSorted(arr);
     }
 }
